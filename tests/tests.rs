b95830bf6f57c16f7b1b0b096d3140ef5cb9be03
rezaarezvan <rezvanr2378@gmail.com>
2023-02-17 18:18:49 +0100

    Add: Unit test for subtract function

--- tests/tests.rs
+++ tests/tests.rs
@@ -20,3 +20,14 @@ fn test_add_vectors() {
     assert_eq!(v3.data, vec![0.0; 3]);
     assert_eq!(v3.data.len(), 3);
 }
+
+#[test]
+fn test_sub_vectors() {
+    let v1 = linear_algebra::Vector::new(3);
+    let v2 = linear_algebra::Vector::new(3);
+
+    let v3 = v1.sub(&v2);
+    assert_eq!(v3.size, 3);
+    assert_eq!(v3.data, vec![0.0; 3]);
+    assert_eq!(v3.data.len(), 3);
+}
