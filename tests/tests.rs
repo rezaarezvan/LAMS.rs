4e10e62c14fc6d09fc4917b43eb29597fdd2e5b9
rezaarezvan <rezvanr2378@gmail.com>
2023-02-28 21:27:56 +0100

    Add: Tensor sub tests (passed)

--- tests/tests.rs
+++ tests/tests.rs
@@ -535,3 +535,66 @@ pub fn test_tensor_add_panic() {
 
     linear_algebra::Tensor::tensor_add(&t1, &t2);
 }
+
+#[test]
+pub fn test_tensor_sub() {
+    let mut t1 = linear_algebra::Tensor::tensor_new(3, 3, 3);
+
+    let mut m1 = linear_algebra::Matrix::new(3, 3);
+    m1.matrix_set(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
+
+    let mut m2 = linear_algebra::Matrix::new(3, 3);
+    m2.matrix_set(vec![9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]);
+
+    let mut m3 = linear_algebra::Matrix::new(3, 3);
+    m3.matrix_set(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
+
+    let mv1 = vec![m1, m2, m3];
+
+    let clone1 = mv1.clone();
+
+    t1.tensor_set(clone1);
+
+    let mut t2 = linear_algebra::Tensor::tensor_new(3, 3, 3);
+
+    let mut m4 = linear_algebra::Matrix::new(3, 3);
+    m4.matrix_set(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
+
+    let mut m5 = linear_algebra::Matrix::new(3, 3);
+    m5.matrix_set(vec![9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]);
+
+    let mut m6 = linear_algebra::Matrix::new(3, 3);
+    m6.matrix_set(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
+
+    let mv2 = vec![m4, m5, m6];
+
+    let clone2 = mv2.clone();
+
+    t2.tensor_set(clone2);
+
+    let expected = mv1
+        .iter()
+        .zip(mv2.iter())
+        .map(|(a, b)| linear_algebra::Matrix::matrix_sub(a, b))
+        .collect::<Vec<_>>();
+
+    let t3 = linear_algebra::Tensor::tensor_sub(&t1, &t2);
+
+    assert_eq!(t3.rank, 3);
+    assert_eq!(t3.rows, 3);
+    assert_eq!(t3.cols, 3);
+
+    // Check that each 3 x 3 corresponds to the correct matrix
+    for i in 0..t3.rank {
+        assert_eq!(t3.data[i].data, expected[i].data);
+    }
+}
+
+#[test]
+#[should_panic]
+pub fn test_tensor_sub_panic() {
+    let t1 = linear_algebra::Tensor::tensor_new(3, 3, 3);
+    let t2 = linear_algebra::Tensor::tensor_new(4, 3, 3);
+
+    linear_algebra::Tensor::tensor_sub(&t1, &t2);
+}
